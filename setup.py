9d1041911d3083e43838343694f892d61db85dc1
Sylwester Kulpa <sylwester.kulpa@gmail.com>
2014-03-26 17:16:29 +0100

    Add author to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['templateit'],
     url='',
     license='',
-    author='ra2er',
-    author_email='sylwester.kulpa@gmail.com',
+    author='ra2er, paluh',
+    author_email='sylwester.kulpa@gmail.com, paluho@gmail.com',
     description=''
 )
